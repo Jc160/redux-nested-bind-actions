de6c3e5272b079e992880e00456c1db5a14fee15
Julio Ramirez <jc16018@gmail.com>
2017-11-25 17:56:41 -0400

    Add redux nested bind actions

---
+++ README.md
@@ -0,0 +1,16 @@
+# Redux Nested Bind Actions
+
+
+Based on Redux bindActionCreator
+
+Turns an object whose values are action creators, into an object with the same keys, but with every function wrapped into a `dispatch` call so they may be invoked directly.
+
+
+## Requirements
+* Redux
+
+
+## Example
+
+
+[bindActionCreators](http://redux.js.org/docs/api/bindActionCreators.html)
---
+++ index.js
@@ -0,0 +1,51 @@
+
+
+/**
+ * This function handles the bind actions creator to the dispatch.
+ * @param  {Function} fnActionCreator Action creator
+ * @param  {Function} fnDispatch      Dispatch
+ * @return {Function}                 Action Dispatcher
+ */
+function bindActionCreator(fnActionCreator, fnDispatch) {
+  return (...listArguments) => fnDispatch(fnActionCreator(...listArguments));
+}
+
+
+
+/**
+ * This function handles the nested action creators parser.
+ * @param  {Variable} varActionCreators Action creators
+ * @param  {Function} fnDispatch        Dispatch
+ * @return {Variable}
+ */
+function bindActionCreators(varActionCreators, fnDispatch) {
+  if (typeof varActionCreators === 'function') {
+    return bindActionCreator(varActionCreators, fnDispatch);
+  }
+
+
+  if (typeof varActionCreators !== 'object' || varActionCreators === null) {
+    throw new Error(
+      `bindActionCreators expected an object or a function, instead received ${actionCreators === null ? 'null' : typeof actionCreators}. ` +
+      `Did you write "import ActionCreators from" instead of "import * as ActionCreators from"?`
+    )
+  }
+
+
+  return Object.keys(varActionCreators).reduce((objActionCreators, strKey) => {
+    const varActionCreator = varActionCreators[strKey];
+    let varResult = null;
+
+    if (typeof varActionCreator === 'function') {
+      varResult = bindActionCreator(varActionCreator, fnDispatch);
+    } else if (typeof varActionCreator === 'object') {
+      varResult = bindActionCreators(varActionCreator, fnDispatch);
+    }
+
+
+    return Object.assign(objActionCreators, { [strKey]: varResult } );
+  }, {});
+}
+
+
+module.exports = bindActionCreators;
